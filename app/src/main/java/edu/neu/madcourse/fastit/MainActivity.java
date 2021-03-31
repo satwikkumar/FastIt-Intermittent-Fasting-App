2aaf324b64697e79162352b5511ef01d25b4d75d
Sagar Ayi <ayisagar@gmail.com>
2021-03-31 18:09:56 -0400

    Capturing and storing pictures from camera

--- app/src/main/java/edu/neu/madcourse/fastit/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/fastit/MainActivity.java
@@ -2,13 +2,26 @@ package edu.neu.madcourse.fastit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textPlan;
     private TextView textLeaderboard;
     private TextView textUserProfile;
+
+    static final int REQUEST_PICTURE_CAPTURE = 1;
+    private String pictureFilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +71,52 @@ public class MainActivity extends AppCompatActivity {
                         return false;
                     }
                 });
+    }
+
+    /*
+    * Picture taking and saving logic
+    * */
+    private void takeImage(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            File pictureFile = null;
+            try {
+                pictureFile = getImageFile();
+            } catch (IOException ex) {
+                showSnackBar("Photo file can't be created, please try again");
+                return;
+            }
+            if (pictureFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.fastit.android.fileprovider",
+                        pictureFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
+            }
+        }
+    }
+
+    private File getImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US).format(new Date());
+        String pictureFile = "FASTIT_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(pictureFile,  ".jpg", storageDir);
+        pictureFilePath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            showSnackBar("Photo created and saved succesfully");
+        }
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message,
+                Snackbar.LENGTH_SHORT)
+                .show();
     }
 
 
