b5599dbb40f13d543a2bcf3099b2d6dc1660a214
Sagar Ayi <ayisagar@gmail.com>
2021-04-05 18:50:25 -0400

    Added logic to store image and weight for a session

--- app/src/main/java/edu/neu/madcourse/fastit/AdditionalInfoActivity.java
+++ app/src/main/java/edu/neu/madcourse/fastit/AdditionalInfoActivity.java
@@ -1,6 +1,7 @@
 package edu.neu.madcourse.fastit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,12 +9,19 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -21,20 +29,33 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class AdditionalInfoActivity extends Activity {
 
+
+    static final int REQUEST_PICTURE_CAPTURE = 1;
     private SharedPreferenceManager sharedPreferenceManager;
     private float weight = 0.0f;
     private String filePath;
     private TextView weightTextView;
+    private ImageView previewImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additional_info);
         sharedPreferenceManager = new SharedPreferenceManager(this);
+        sharedPreferenceManager.setLongPref(Constants.SP_CURRENT_FASTING_END_TIME, System.currentTimeMillis());
         weightTextView = findViewById(R.id.weight_text);
         weightTextView.setText(weight+" kg");
+        previewImage = findViewById(R.id.image_preview);
     }
 
     public void dismissActivity(View view){
@@ -115,5 +136,55 @@ public class AdditionalInfoActivity extends Activity {
 
     public void enterWeightPressed(View view){
         addNewUser();
+    }
+
+    public void launchCamera(View view){
+        takeImage();
+    }
+
+
+    private void takeImage(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            File pictureFile = null;
+            try {
+                pictureFile = getImageFile();
+            } catch (IOException ex) {
+                showSnackBar("Photo file can't be created, please try again");
+                return;
+            }
+            if (pictureFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.fastit.android.fileprovider",
+                        pictureFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, REQUEST_PICTURE_CAPTURE);
+            }
+        }
+    }
+
+    private File getImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US).format(new Date());
+        String pictureFile = "FASTIT_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(pictureFile,  ".jpg", storageDir);
+        filePath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PICTURE_CAPTURE && resultCode == RESULT_OK) {
+            Uri photoURI = Uri.parse(filePath);
+            Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+            previewImage.setImageBitmap(bitmap);
+        }
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message,
+                Snackbar.LENGTH_SHORT)
+                .show();
     }
 }
--- app/src/main/java/edu/neu/madcourse/fastit/Constants.java
+++ app/src/main/java/edu/neu/madcourse/fastit/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static String SP_CURRENT_FASTING_CYCLE = "current_fasting_cycle";
     public static String SP_CURRENT_WEIGHT = "current_weight";
     public static String SP_CURRENT_IMAGE_PATH = "current_image_path";
+    public static String SP_CURRENT_FASTING_END_TIME = "current_fasting_end_time";
+    public static String SP_CURRENT_FASTING_START_TIME = "current_fasting_start_time";
 }
